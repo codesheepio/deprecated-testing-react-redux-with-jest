89a25d464631bdf7a77a502f3939150059e887ff
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-02 01:50:28 +0700

    Test TodoList has correct structure

---
+++ src/components/__tests__/TodoList.test.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { shallow } from 'enzyme' // eslint-disable-line import/no-extraneous-dependencies
+import toJson from 'enzyme-to-json' // eslint-disable-line import/no-extraneous-dependencies
+import TodoList from '../TodoList'
+
+describe('TodoList', () => {
+  let wrapper
+  let props
+
+  beforeEach(() => {
+    props = {
+      todos: [
+        { id: 1, text: 'Feed cat', complete: false },
+        { id: 2, text: 'Drink coffee', complete: true },
+      ],
+    }
+    wrapper = shallow(<TodoList {...props} />)
+  })
+
+  it('matches its snapshot - empty', () => {
+    wrapper.setProps({ todos: [] })
+
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('matches its snapshot - with todos', () => {
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('renders correct structure', () => {
+    expect(wrapper.is('ul')).toBe(true)
+    expect(wrapper.hasClass('todo-list')).toBe(true)
+  })
+})
