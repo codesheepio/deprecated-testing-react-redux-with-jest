2f28a87a907cd1074e2506518eb0720e29657a39
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-02 02:00:28 +0700

    Test TodoList renders TodoItem from props

--- src/components/__tests__/TodoList.test.js
+++ src/components/__tests__/TodoList.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme' // eslint-disable-line import/no-extraneous-dependencies
 import toJson from 'enzyme-to-json' // eslint-disable-line import/no-extraneous-dependencies
 import TodoList from '../TodoList'
+import TodoItem from '../TodoItem'
 
 describe('TodoList', () => {
   let wrapper
@@ -30,5 +31,9 @@ describe('TodoList', () => {
   it('renders correct structure', () => {
     expect(wrapper.is('ul')).toBe(true)
     expect(wrapper.hasClass('todo-list')).toBe(true)
+  })
+
+  it('renders todo items from props', () => {
+    expect(wrapper.find(TodoItem).length).toBe(props.todos.length)
   })
 })
