00a86fd799c25ac94196eb27fbf398bdb38a833c
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-02 01:17:05 +0700

    test classname

--- src/components/__tests__/TodoItem.test.js
+++ src/components/__tests__/TodoItem.test.js
@@ -22,4 +22,10 @@ describe('TodoItem', () => {
     expect(wrapper.find('li').length).toBe(1)
     expect(wrapper.find('li').at(0).text()).toEqual('Drink coffee')
   })
+
+  it('has complete className if receiving complete from props', () => {
+    const wrapper = shallow(<TodoItem text="Drink coffee" complete />)
+
+    expect(wrapper.find('li').at(0).hasClass('complete')).toBe(true)
+  })
 })
