fb9f0b8bdb6de3cc473198dedebece2ac3a7e99c
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-02 02:40:33 +0700

    Test mapStateToProps

--- src/containers/__tests__/TodoList.test.js
+++ src/containers/__tests__/TodoList.test.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { mount } from 'enzyme' // eslint-disable-line import/no-extraneous-dependencies
 import TodoListContainer from '../TodoList'
 import TodoList from '../../components/TodoList'
+import { mapStateToProps } from '../TodoList'
 
 describe('TodoList Container', () => {
   const createStore = configureMockStore()
@@ -29,5 +30,21 @@ describe('TodoList Container', () => {
       { id: 1, text: 'Feed cat', complete: false },
       { id: 2, text: 'Drink coffee', complete: true },
     ])
+  })
+
+  test('mapStateToProps performs correctly', () => {
+    const state = {
+      todos: [
+        { id: 1, text: 'Feed cat', complete: false },
+        { id: 2, text: 'Drink coffee', complete: true },
+      ],
+    }
+    const expectedProps = {
+      todos: [
+        { id: 1, text: 'Feed cat', complete: false },
+        { id: 2, text: 'Drink coffee', complete: true },
+      ],
+    }
+    expect(mapStateToProps(state)).toEqual(expectedProps)
   })
 })
