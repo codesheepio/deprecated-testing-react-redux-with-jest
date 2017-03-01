14049eb78ddd0414acc0578f9179f1422763d886
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-02 02:54:35 +0700

    Test todosReducer to handle addTodo action

--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -1,5 +1,20 @@
+import { ADD_TODO } from '../actions/types'
+
 const todosReducer = (state = [], action) => {
   switch (action.type) {
+    case ADD_TODO: {
+      const nextId = state.reduce((max, todo) =>
+        (Math.max(max, todo.id)), 0) + 1
+
+      return [
+        ...state,
+        {
+          id: nextId,
+          text: action.payload.text,
+          complete: false,
+        },
+      ]
+    }
     default:
       return state
   }
