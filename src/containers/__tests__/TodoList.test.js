8afb419bd9593beae5d28dcbaa5929b8e294eb9f
Supasate Choochaisri <supasate.c@gmail.com>
2017-03-02 02:31:14 +0700

    Test TodoList container to receives props from state

---
+++ src/containers/__tests__/TodoList.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import configureMockStore from 'redux-mock-store' // eslint-disable-line import/no-extraneous-dependencies
+import { Provider } from 'react-redux'
+import { mount } from 'enzyme' // eslint-disable-line import/no-extraneous-dependencies
+import TodoListContainer from '../TodoList'
+import TodoList from '../../components/TodoList'
+
+describe('TodoList Container', () => {
+  const createStore = configureMockStore()
+  let wrapper
+
+  beforeEach(() => {
+    const state = {
+      todos: [
+        { id: 1, text: 'Feed cat', complete: false },
+        { id: 2, text: 'Drink coffee', complete: true },
+      ],
+    }
+    const store = createStore(state)
+    wrapper = mount(
+      <Provider store={store}>
+        <TodoListContainer />
+      </Provider>
+    ).find(TodoListContainer).find(TodoList)
+  })
+
+  it('receives todos props from state', () => {
+    expect(wrapper.prop('todos')).toEqual([
+      { id: 1, text: 'Feed cat', complete: false },
+      { id: 2, text: 'Drink coffee', complete: true },
+    ])
+  })
+})
